d3353c90863eff24f969c2568a77eef13fcc3262
Mantis <tangchunlinit@163.com>
2018-12-03 21:59:42 +0800

    save template.

--- src/page/article/index/save.jsx
+++ src/page/article/index/save.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import {Form, Input, Select, Button, Checkbox, Breadcrumb, Radio} from 'antd';
+import {Link} from "react-router-dom";
+import {Form, Input, Select, Button, Checkbox, Breadcrumb, Icon, Upload, Switch} from 'antd';
+
+import Editor from 'util/editor/index.jsx';
 
 const FormItem = Form.Item;
 const Option = Select.Option;
 const CheckboxGroup = Checkbox.Group;
-const RadioGroup = Radio.Group;
+const {TextArea} = Input;
 
 import './index.scss';
-import {Link} from "react-router-dom";
 
 const routes = [{
     path: '/',
@@ -25,6 +27,24 @@ function itemRender(route, params, routes, paths) {
     return last ? <span>{route.breadcrumbName}</span> : <Link to={paths.join('/')}>{route.breadcrumbName}</Link>;
 }
 
+function getBase64(img, callback) {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => callback(reader.result));
+    reader.readAsDataURL(img);
+}
+
+function beforeUpload(file) {
+    const isJPG = file.type === 'image/jpeg';
+    if (!isJPG) {
+        message.error('You can only upload JPG file!');
+    }
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+        message.error('Image must smaller than 2MB!');
+    }
+    return isJPG && isLt2M;
+}
+
 const plainOptions = ['PHP', 'Golang', 'Java', 'C++', 'Python'];
 const defaultCheckedList = [];
 
@@ -35,7 +55,8 @@ class Save extends React.Component {
             checkedList: defaultCheckedList,
             indeterminate: true,
             checkAll: false,
-            publish: 1
+            publish: 1,
+            loading: false,
         }
     }
 
@@ -62,6 +83,39 @@ class Save extends React.Component {
     }
 
     render() {
+        const formItemLayout = {
+            labelCol: {
+                xs: {span: 24},
+                sm: {span: 7}
+            },
+            wrapperCol: {
+                xs: {span: 24},
+                sm: {span: 12}
+            }
+        };
+
+        const tailFormItemLayout = {
+            wrapperCol: {
+                xs: {
+                    span: 24,
+                    offset: 0,
+                },
+                sm: {
+                    span: 16,
+                    offset: 17,
+                },
+            },
+        };
+
+        const uploadButton = (
+            <div>
+                <Icon type={this.state.loading ? 'loading' : 'cloud-upload'}/>
+                <div className="ant-upload-text">上传图片</div>
+            </div>
+        );
+
+        const imageUrl = this.state.imageUrl;
+
         return (
             <div className="debon-article-list-container">
                 <div className="debon-component-page-header">
@@ -81,17 +135,17 @@ class Save extends React.Component {
                             <div className="ant-card-body">
                                 <div className="debon-component-pages-list-table-list-tableList">
                                     <Form layout="horizontal">
-                                        <FormItem label="标题">
+                                        <FormItem {...formItemLayout} label="标题">
                                             <Input placeholder="文章标题"/>
                                         </FormItem>
-                                        <FormItem label="分类">
+                                        <FormItem {...formItemLayout} label="分类">
                                             <Select showSearch placeholder="请选择分类" optionFilterProp="children"
                                                     filterOption={(input, open) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}>
                                                 <Option value="1">后端</Option>
                                                 <Option value="2">前端</Option>
                                             </Select>
                                         </FormItem>
-                                        <FormItem label="标签">
+                                        <FormItem {...formItemLayout} label="标签">
                                             <Checkbox indeterminate={this.state.indeterminate}
                                                       onChange={(e) => this.onCheckAllChange(e)}
                                                       checked={this.state.checkAll}>全选</Checkbox>
@@ -99,12 +153,24 @@ class Save extends React.Component {
                                                            value={this.state.checkedList}
                                                            onChange={(e) => this.onChange(e)}/>
                                         </FormItem>
-                                        <FormItem label="发布">
-                                            <RadioGroup onChange={(e) => this.onPublishChange(e)}
-                                                        value={this.state.publish}>
-                                                <Radio value={1}>是</Radio>
-                                                <Radio value={0}>否</Radio>
-                                            </RadioGroup>
+                                        <FormItem {...formItemLayout} label="简介">
+                                            <TextArea rows="4"/>
+                                        </FormItem>
+                                        <FormItem {...formItemLayout} label="封面">
+                                            <Upload name="bg_img" listType="picture-card" className="avatar-uploader"
+                                                    showUploadList={false} action="">
+                                                {imageUrl ? <img src={imageUrl} alt="bg_img"/> : uploadButton}
+                                            </Upload>
+                                        </FormItem>
+                                        <FormItem {...formItemLayout} label="内容">
+                                            <Editor/>
+                                        </FormItem>
+                                        <FormItem {...formItemLayout} label="发布">
+                                            <Switch checkedChildren={<Icon type="check"/>}
+                                                    unCheckedChildren={<Icon type="close"/>} defaultChecked/>
+                                        </FormItem>
+                                        <FormItem {...tailFormItemLayout}>
+                                            <Button type="primary" htmlType="button"><Icon type="save"/>保存</Button>
                                         </FormItem>
                                     </Form>
                                 </div>
